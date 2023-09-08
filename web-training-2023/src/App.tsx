2f6e71b8ea772bca79aef14129f3aee11c5ab056
exampleCoderOne <exampleemail3001@gmail.com>
2023-09-08 15:43:31 -0700

    setting up project

---
+++ web-training-2023/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>[your app name here]</h1>
+      <div className="card">
+        <p>
+          Time to make your very own quiz.
+        </p>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
