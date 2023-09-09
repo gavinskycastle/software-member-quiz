5e0a0d6211fae1a4d78fc5341300c52ea22763ec
exampleCoderOne <exampleemail3001@gmail.com>
2023-09-08 17:12:20 -0700

    added input

--- web-training-2023/src/App.tsx
+++ web-training-2023/src/App.tsx
@@ -1,17 +1,32 @@
-import './App.css'
+import React from "react";
+import "./App.css";
+import { useState } from 'react'
 
 function App() {
+  const [answers, setAnswers] = useState([])
+
+  // function SetAnswers(){
+  //   <input type="text" value="Answer to Question One"></input>
+  // }
+
   return (
     <>
-      <h1>[your app name here]</h1>
+      <h1>Quiz about Data Science!</h1>
       <div className="card">
-        <p>
-          Time to make your very own quiz.
-        </p>
+        <p>Time to make your very own quiz.</p>
+
+        <input type="text" placeholder="Answer to question one"/>
+        <button>Submit</button>
+
+       
+
+  
+  
+
+     
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
