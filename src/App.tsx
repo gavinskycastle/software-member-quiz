39df9007c16e0085fd9af50de46ddf6d06c59a48
Josiah Fu <Josiah Fu>
2023-09-12 20:20:53 -0700

    Milad's training code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,45 @@
 import './App.css';
+import { useState } from 'react';
+import Question from './Question';
 
 function App() {
+    const [answer1, setAnswer1] = useState<string>();
+    const [answer2, setAnswer2] = useState<string>();
+    const [answer3, setAnswer3] = useState<string>();
+    const [answer4, setAnswer4] = useState<string>();
+    const[colorChanged, setColorChanged] = useState(false);
+
     return (
         <>
-            <h1>[your app name here]</h1>
-            <p>Time to make your very own quiz.</p>
-        </>
+        <h1 className={colorChanged ?'changed' : ''}>Penguin Quiz</h1>
+            <h1>[HARDEST QUIZ EVER]</h1>
+            <p>Time to make your very own sherizz.</p>
+            {/*comment*/}
+            <div>
+                <input />
+            </div>
+            
+            <Question title='1. What color are people who rizz up?' onChange={setAnswer1}/>
+            <Question title='2. What color is the girl your rizzing?'onChange={setAnswer2}/> 
+            <Question title='3. Is Kyles color of rizz diffrent?'onChange={setAnswer3}/>
+            <Question title='4. did dyaln rizz up any girls at the soccer tryouts?'onChange={setAnswer4}/>
+                <h2> Question 1: What color is rizz?</h2>
+                <p> This is also a paragraph</p>
+            
+
+            <h2> Question:2 What is rizz?</h2>
+            <input />
+
+            <div> 
+                <button>Check Answers</button>
+            </div>
+            <button onClick={() => setColorChanged(true)}>anything</button>
+            <h2>Answers that you typed</h2>
+            <p>{answer1}</p>
+            <p>{answer2}</p>
+            <p>{answer3}</p>
+            <p>{answer4}</p>
+            </>
     );
 }
 
---
+++ src/Question.tsx
@@ -0,0 +1,16 @@
+import handleInput from "./handleInput";
+
+function Question(props: {
+    title: string
+    onChange: (value: string) => void
+}) {
+     return (<>
+            <h2>{props.title}</h2>
+            <div>
+                <input onChange={handleInput(props.onChange)}/>
+            </div>
+        </>
+    );
+}
+
+export default Question;
